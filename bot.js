cabe93fcd66747714be5cba985c8b960b719ef6e
benmozes <s.v.kremen@gmail.com>
2016-07-15 16:37:08 +0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -84,7 +84,7 @@ bot.onText(/(.+)$/, function (msg, match) {
                 {
                     // add the line break if not the first answer
                     if (itemsFound==0) 
-                        formattedAnswer += "Что это за покемон?" + '\u2753';
+                        formattedAnswer += "<b>Что это за покемон</b>" + 'u2753';
                     else 
                         formattedAnswer += "\n\n";
                         
