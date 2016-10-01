017ff21a518d5173fd20e663030ed3b7168c517d
benmozes <s.v.kremen@gmail.com>
2016-10-01 12:56:16 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,4 @@
 module.exports = {
- 'TelegramProductionURL': 'https://myapp-bot-app.herokuapp.com/', // do not forget trailing "/" !!
- 'TelegramToken': '242770280:AAE_vg6W5CCGBgwQnTKc3YGBPUnShc7zJAE', // Telegram token
- 'googleSheetKey': '1QiEwplSQsEdWHUiSCa9uzrKph_NXTbUY9tEE32bc-Eg', // the key of the google sheet (should be public!), extract key from the google sheet doc publich url
- "confTimeZone": "Asia/Aqtau" // time zone of the conference in the form like "Europe/Berlin", see http://momentjs.com/timezone/
+ 'TelegramProductionURL': 'https://mysticchellybot.herokuapp.com/', // do not forget trailing "/" !!
+ 'TelegramToken': '157513816:AAGXrZfyp6isZzv4GQsKYajhEknwT20-W3w' // Telegram token
 }
